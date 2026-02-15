cee725bba841fd8f4c73238a3dfc7086ca58c7b1
u1184009 <u1184009@umail.utah.edu>
2026-02-15 15:40:21 -0700

    Initial COmmit

---
+++ PasswordChecking.cs
@@ -0,0 +1,12 @@
+﻿namespace BlockProject2;
+/**
+ * This program is a simple password checker and encryptor
+ *
+ * author: Mike Pham
+ *
+ * 2026-19-2
+ */
+public class PasswordChecking
+{
+    
+}
