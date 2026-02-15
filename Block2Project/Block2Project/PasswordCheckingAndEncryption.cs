dd5b14bc77e00e23dc79b676c1e15893b0ee388b
u1184009 <u1184009@umail.utah.edu>
2026-02-15 16:00:37 -0700

    Password checking commit

---
+++ Block2Project/Block2Project/PasswordCheckingAndEncryption.cs
@@ -0,0 +1,12 @@
+namespace Block2Project;
+/*
+ * Simple Program to take in password, check, and encrypt it
+ *
+ * By Mike Pham
+ *
+ * 2026-19-2
+ */
+public class PasswordCheckingAndEncryption
+{
+    
+}
